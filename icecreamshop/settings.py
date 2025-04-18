099b20225b96c81f3b9ee8093195cdf8a7004059
Halomedicals Systems <halomedicalssystems@Halomedicalss-MacBook-Pro.local>
2025-04-19 00:19:56 +0100

    fifth commit fix

--- icecreamshop/settings.py
+++ icecreamshop/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'orders',
     'tailwind',
     'theme',
-    'django_browser_reload',
 ]
 
 MIDDLEWARE = [
@@ -52,7 +51,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django_browser_reload.middleware.BrowserReloadMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
